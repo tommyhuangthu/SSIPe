7907d7f546a528dcaa965c83b584e5d733bd1763
tommyhuangthu <36273004+tommyhuangthu@users.noreply.github.com>
2019-10-24 14:00:43 -0400

    Add files via upload
    The get_ssip_score.pl calls other scripts and binaries to calculate ddG_SSIP score.

---
+++ ssip/get_ssip_score.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+#################################################################################
+# this script is used to call the SSIP program to calculate the profile score
+# Input:  
+#         mutList.txt          # contains the mutants that
+#         structure_align.out  # interface structure alignment file by iAlign 
+#         sequence_align.out   # interface sequence alignment file by PSI-BLAST
+#         isscore              # a float value (0, 1] for scoring
+# Output: 
+#         ssip_score.txt       #predicted scores, one ddG each line
+#################################################################################
+
+use strict;
+use warnings;
+
+use File::Basename;
+use Cwd 'abs_path';
+
+if(@ARGV != 5){
+  printf "Usage: get_ssip_score.pl mutList.txt structure_align.out sequence_align.out isscore ssip_score.txt\n";
+  exit(-1);
+}
+
+
+my $bin_path=dirname(abs_path(__FILE__));
+
+my $mut_list        = $ARGV[0];
+my $structure_align = $ARGV[1];
+my $sequence_align  = $ARGV[2];
+my $isscore         = $ARGV[3];
+my $ssip_score      = $ARGV[4];
+
+
+
+my $ssippath      = "$bin_path/SSIPserver";
+my $sort_ialign   = "$bin_path/sort_ialign.py";
+my $sort_seqalign = "bin_path/sort_seqalign.py"; 
+
+#sort the structure alignment
+`$sort_ialign $structure_align $structure_align.sort`;
+`mv $structure_align.sort $structure_align`;
+`cp -r $ssippath/parameter .`;
+`$ssippath/SSIP --command=DDG  --isscore=$isscore  --mutant_file=$mut_list --structure_align=$structure_align --sequence_align=$sequence_align --ssipscore_file=$ssip_score`;
+if(-d "./parameter"){
+  `rm -rf parameter`;
+}
+exit;
---
+++ ssip/run_ialign.pl
@@ -0,0 +1,282 @@
+#!/usr/bin/perl
+use strict;
+use Switch;
+use File::Basename;
+use Cwd 'abs_path';
+
+#####HELP MESSAGE############################
+if (@ARGV<3)
+{
+	die "run_align.pl DIMER_PDB_FILE IS-SCORE_CUTOFF ALIGNMENT_OUTPUT_FILE\n";
+}
+#############################################
+
+my $maxLenth=16;
+my @a = (0..9,'a'..'z','A'..'Z');
+
+my $path=dirname(abs_path(__FILE__));
+
+my $randStr = join '', map { $a[int rand @a] } 0..($maxLenth-1);
+my $randName = "X_".$randStr.".pdb";
+my $tmpResult = "X_".$randStr.".ial";
+
+
+my $queryDimer=$ARGV[0];
+my $is_cutoff=$ARGV[1];
+my $outputFile=$ARGV[2];
+
+my $ialign_type="is";
+my $libList="$path/NIL/dimer.list";
+my $libPath="$path/NIL";
+my $IALIGN="$path/ialign/ialign.pl";
+my $parseFile="X_".$randStr.".parsed";
+
+#####PARAMETERS FROM LAUNCH INTERFACE########
+my $mini='7';
+my $minp='7';
+my $dist_cutoff=4.5;
+#############################################
+
+`cp $queryDimer $randName`;
+
+print "$IALIGN -a 2 -n ave -s -minp $minp -mini $mini -dc $dist_cutoff -e $ialign_type -w $libPath -l $libList $randName >$tmpResult\n";
+`$IALIGN -a 2 -n ave -s -minp $minp -mini $mini -dc $dist_cutoff -e $ialign_type -w $libPath -l $libList $randName >$tmpResult`;
+
+
+my $chainAB="XX";
+
+open(IALTMP, "<$tmpResult");
+while(my $line = <IALTMP>)
+{
+	if ( $line =~ /$parseFile/ )
+	{
+		chomp(my $ifNum=(split /[\s+,]/, $line)[2]);
+		if($ifNum == 0)
+		{
+			die "no interface for ".$queryDimer;
+		}
+		elsif($ifNum > 1)
+		{
+			die "too many interface for ".$queryDimer;
+		}
+		chomp($chainAB=(split /[\s+,]/, $line)[5]);
+		last;
+	}
+}
+
+#print "$chainAB\n";
+close IALTMP;
+
+my $chainA=substr($chainAB,0,1);
+my $chainB=substr($chainAB,1,1);
+
+
+my $intFile=$libPath."/X_".$randStr.$chainAB."_int.pdb";
+my $conFile=$libPath."/X_".$randStr.$chainAB."_con.lst";
+if (! -s $intFile){
+	$intFile="X_".$randStr.$chainAB."_int.pdb";
+}
+if (!-s $conFile){
+	$conFile="X_".$randStr.$chainAB."_con.lst";
+}
+open (INTF, "<$intFile") || die "can't open intface pdb file";
+
+my @resAArray;
+my @resBArray;
+my $resAIndex=0;
+my $resBIndex=0;
+my $querySeqA="";
+my $querySeqB="";
+while(my $line = <INTF>)
+{
+	if( $line =~ /ATOM/ )
+	{
+		my $ch = substr($line,21,1);
+		my $resID = substr($line,22,5);
+		chomp($resID);
+		if( $ch eq $chainA)
+		{
+			@resAArray[$resAIndex]=$resID;
+			my $type=changeAA(substr($line,17,3));
+			$querySeqA=$querySeqA.$type;
+			$resAIndex++;
+		}
+		if( $ch eq $chainB)
+		{
+			@resBArray[$resBIndex]=$resID;
+			my $type=changeAA(substr($line,17,3));
+                        $querySeqB=$querySeqB.$type;
+			$resBIndex++;
+		}
+	}
+}
+
+my $ifResNum=$resAIndex + $resBIndex;
+
+#print "$querySeqA\n$querySeqB\n";
+
+open (OUT, ">$outputFile");
+
+print OUT "Interface residues:\n";
+print OUT "chain_ID_A: ".$chainA."\n";
+print OUT "residue_A: ";
+
+foreach my $resID (@resAArray)
+{
+	print OUT "$resID ";
+}
+print OUT "\n";
+
+print OUT "chain_ID_B: ".$chainB."\n";
+print OUT "residue_B: ";
+foreach my $resID (@resBArray)
+{
+        print OUT "$resID ";
+}
+print OUT "\n";
+
+print OUT "Alignments:\n";
+
+print OUT "PDB   chA chB ifResNum alnResNum score ifSeqA ifSeqB\n";
+
+printf OUT ("query  $chainA   $chainB  %3d  %3d  %5.3f  %s %s\n",$ifResNum,$ifResNum,1.000,$querySeqA,$querySeqB);
+
+
+my $libIndex=0;
+my $score=0.0;
+my $libSeqA;
+my $libSeqB;
+my $libIfNum;
+my $libAlnNum;
+my $libChA;
+my $libChB;
+my $resAlignIndex=0;
+my @libResA;
+my @libResB;
+my $libPDBID;
+
+open(IALTMP, "<$tmpResult");
+while(my $line = <IALTMP>)
+{
+	if( $line =~ /Structure 1:/)
+	{
+		$libChA=substr($line,41,1);
+		$libChB=substr($line,43,1);
+		$libPDBID=substr($line,13,6);
+		$libIfNum=substr($line,46,3);
+		next;
+	}
+	if( $line =~ /aligned residues/)
+	{
+		$libAlnNum=substr($line,30,3);
+	}
+	if( ($line =~ /IS-score/) || ($line =~ /TM-score/) )
+	{
+		$libIndex = $libIndex+1;
+		$libSeqA="";
+		$libSeqB="";
+		$resAlignIndex=0;
+		for(my $i=0;$i<$resAIndex;$i++)
+		{
+			@libResA[$i]='-';
+		}
+		for(my $i=0;$i<$resBIndex;$i++)
+                {
+                        @libResB[$i]='-';
+                }
+
+		chomp($score=(split /[\s+,]/, $line)[2]);
+#		print "$score\n";
+		next;
+	}
+        if($score < $is_cutoff)
+        {
+                next;
+        }
+
+	if( $line =~ / Index /)
+	{
+		$resAlignIndex=1;
+		next;
+	}
+	if( $line =~ / Ansgtrom /)
+	{
+		$resAlignIndex=0;
+	
+		printf OUT ("%s $libChA   $libChB  %s  %s  %5.3f  ", $libPDBID, $libIfNum , $libAlnNum, $score);  
+		for(my $i=0;$i<$resAIndex;$i++)
+		{
+			print OUT @libResA[$i];
+		}
+		print OUT " ";
+		for(my $i=0;$i<$resBIndex;$i++)
+                {
+                        print OUT @libResB[$i];
+                }
+		print OUT "\n";
+		next;
+	}
+	if($resAlignIndex > 0)
+	{
+		my $quResID=substr($line,29,5);
+		my $libResType=changeAA(substr($line,18,3));
+		if( $chainA eq substr($line,27,1))
+		{
+			my $idx=searchIndex($quResID, $resAIndex, @resAArray);
+			$libChA=substr($line,9,1);
+			@libResA[$idx]=$libResType;
+		}
+		if ( $chainB eq substr($line,27,1))
+		{
+			my $idx=searchIndex($quResID, $resBIndex, @resBArray);
+			$libChB=substr($line,9,1);
+			@libResB[$idx]=$libResType;
+		}
+	}	
+
+}
+
+sub changeAA {
+        my $triName=@_[0];
+        switch ($triName){
+                case "ALA"  {return 'A'}
+                case "ARG"  {return 'R'}
+                case "ASP"  {return 'D'}
+                case "ASN"  {return 'N'}
+                case "CYS"  {return 'C'}
+                case "GLN"  {return 'Q'}
+                case "GLY"  {return 'G'}
+                case "GLU"  {return 'E'}
+                case "HIS"  {return 'H'}
+                case "ILE"  {return 'I'}
+                case "LEU"  {return 'L'}
+                case "LYS"  {return 'K'}
+                case "MET"  {return 'M'}
+                case "PHE"  {return 'F'}
+                case "PRO"  {return 'P'}
+                case "SER"  {return 'S'}
+                case "THR"  {return 'T'}
+                case "TRP"  {return 'W'}
+                case "TYR"  {return 'Y'}
+                case "VAL"  {return 'V'}
+        }
+}
+
+sub searchIndex {
+	my $query=@_[0];
+	my $num=@_[1];
+	for(my $i=0;$i<$num;$i++)
+	{
+		if($query eq @_[$i+2])
+		{	
+			return $i;
+		}
+	}
+	die "can't find residue ".$query;
+}
+
+
+`rm $randName`;
+`rm $tmpResult`;
+`rm $intFile`;
+`rm $libPath/X_$randStr*`;
