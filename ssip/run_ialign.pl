ef5bb83ff3aedb5a46f380136198efbd5f25efe4
tommyhuangthu <tommyhuangthu@foxmail.com>
2019-11-07 19:27:08 -0500

    update the /SSIPe/ssip/run_ialign.pl

--- ssip/run_ialign.pl
+++ ssip/run_ialign.pl
@@ -1,13 +1,12 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use strict;
-use Switch;
 use File::Basename;
 use Cwd 'abs_path';
 
 #####HELP MESSAGE############################
 if (@ARGV<3)
 {
-	die "run_align.pl DIMER_PDB_FILE IS-SCORE_CUTOFF ALIGNMENT_OUTPUT_FILE\n";
+  die "run_align.pl DIMER_PDB_FILE IS-SCORE_CUTOFF ALIGNMENT_OUTPUT_FILE\n";
 }
 #############################################
 
@@ -48,20 +47,20 @@ my $chainAB="XX";
 open(IALTMP, "<$tmpResult");
 while(my $line = <IALTMP>)
 {
-	if ( $line =~ /$parseFile/ )
-	{
-		chomp(my $ifNum=(split /[\s+,]/, $line)[2]);
-		if($ifNum == 0)
-		{
-			die "no interface for ".$queryDimer;
-		}
-		elsif($ifNum > 1)
-		{
-			die "too many interface for ".$queryDimer;
-		}
-		chomp($chainAB=(split /[\s+,]/, $line)[5]);
-		last;
-	}
+  if ( $line =~ /$parseFile/ )
+  {
+    chomp(my $ifNum=(split /[\s+,]/, $line)[2]);
+    if($ifNum == 0)
+    {
+      die "no interface for ".$queryDimer;
+    }
+    elsif($ifNum > 1)
+    {
+      die "too many interface for ".$queryDimer;
+    }
+    chomp($chainAB=(split /[\s+,]/, $line)[5]);
+    last;
+  }
 }
 
 #print "$chainAB\n";
@@ -74,10 +73,10 @@ my $chainB=substr($chainAB,1,1);
 my $intFile=$libPath."/X_".$randStr.$chainAB."_int.pdb";
 my $conFile=$libPath."/X_".$randStr.$chainAB."_con.lst";
 if (! -s $intFile){
-	$intFile="X_".$randStr.$chainAB."_int.pdb";
+  $intFile="X_".$randStr.$chainAB."_int.pdb";
 }
 if (!-s $conFile){
-	$conFile="X_".$randStr.$chainAB."_con.lst";
+  $conFile="X_".$randStr.$chainAB."_con.lst";
 }
 open (INTF, "<$intFile") || die "can't open intface pdb file";
 
@@ -89,26 +88,26 @@ my $querySeqA="";
 my $querySeqB="";
 while(my $line = <INTF>)
 {
-	if( $line =~ /ATOM/ )
-	{
-		my $ch = substr($line,21,1);
-		my $resID = substr($line,22,5);
-		chomp($resID);
-		if( $ch eq $chainA)
-		{
-			@resAArray[$resAIndex]=$resID;
-			my $type=changeAA(substr($line,17,3));
-			$querySeqA=$querySeqA.$type;
-			$resAIndex++;
-		}
-		if( $ch eq $chainB)
-		{
-			@resBArray[$resBIndex]=$resID;
-			my $type=changeAA(substr($line,17,3));
+  if( $line =~ /ATOM/ )
+  {
+    my $ch = substr($line,21,1);
+    my $resID = substr($line,22,5);
+    chomp($resID);
+    if( $ch eq $chainA)
+    {
+      $resAArray[$resAIndex]=$resID;
+      my $type=changeAA(substr($line,17,3));
+      $querySeqA=$querySeqA.$type;
+      $resAIndex++;
+    }
+    if( $ch eq $chainB)
+    {
+      $resBArray[$resBIndex]=$resID;
+      my $type=changeAA(substr($line,17,3));
                         $querySeqB=$querySeqB.$type;
-			$resBIndex++;
-		}
-	}
+      $resBIndex++;
+    }
+  }
 }
 
 my $ifResNum=$resAIndex + $resBIndex;
@@ -123,7 +122,7 @@ print OUT "residue_A: ";
 
 foreach my $resID (@resAArray)
 {
-	print OUT "$resID ";
+  print OUT "$resID ";
 }
 print OUT "\n";
 
@@ -158,121 +157,119 @@ my $libPDBID;
 open(IALTMP, "<$tmpResult");
 while(my $line = <IALTMP>)
 {
-	if( $line =~ /Structure 1:/)
-	{
-		$libChA=substr($line,41,1);
-		$libChB=substr($line,43,1);
-		$libPDBID=substr($line,13,6);
-		$libIfNum=substr($line,46,3);
-		next;
-	}
-	if( $line =~ /aligned residues/)
-	{
-		$libAlnNum=substr($line,30,3);
-	}
-	if( ($line =~ /IS-score/) || ($line =~ /TM-score/) )
-	{
-		$libIndex = $libIndex+1;
-		$libSeqA="";
-		$libSeqB="";
-		$resAlignIndex=0;
-		for(my $i=0;$i<$resAIndex;$i++)
-		{
-			@libResA[$i]='-';
-		}
-		for(my $i=0;$i<$resBIndex;$i++)
+  if( $line =~ /Structure 1:/)
+  {
+    $libChA=substr($line,41,1);
+    $libChB=substr($line,43,1);
+    $libPDBID=substr($line,13,6);
+    $libIfNum=substr($line,46,3);
+    next;
+  }
+  if( $line =~ /aligned residues/)
+  {
+    $libAlnNum=substr($line,30,3);
+  }
+  if( ($line =~ /IS-score/) || ($line =~ /TM-score/) )
+  {
+    $libIndex = $libIndex+1;
+    $libSeqA="";
+    $libSeqB="";
+    $resAlignIndex=0;
+    for(my $i=0;$i<$resAIndex;$i++)
+    {
+      $libResA[$i]='-';
+    }
+    for(my $i=0;$i<$resBIndex;$i++)
                 {
-                        @libResB[$i]='-';
+                        $libResB[$i]='-';
                 }
 
-		chomp($score=(split /[\s+,]/, $line)[2]);
-#		print "$score\n";
-		next;
-	}
+    chomp($score=(split /[\s+,]/, $line)[2]);
+#    print "$score\n";
+    next;
+  }
         if($score < $is_cutoff)
         {
                 next;
         }
 
-	if( $line =~ / Index /)
-	{
-		$resAlignIndex=1;
-		next;
-	}
-	if( $line =~ / Ansgtrom /)
-	{
-		$resAlignIndex=0;
-	
-		printf OUT ("%s $libChA   $libChB  %s  %s  %5.3f  ", $libPDBID, $libIfNum , $libAlnNum, $score);  
-		for(my $i=0;$i<$resAIndex;$i++)
-		{
-			print OUT @libResA[$i];
-		}
-		print OUT " ";
-		for(my $i=0;$i<$resBIndex;$i++)
+  if( $line =~ / Index /)
+  {
+    $resAlignIndex=1;
+    next;
+  }
+  if( $line =~ / Ansgtrom /)
+  {
+    $resAlignIndex=0;
+  
+    printf OUT ("%s $libChA   $libChB  %s  %s  %5.3f  ", $libPDBID, $libIfNum , $libAlnNum, $score);  
+    for(my $i=0;$i<$resAIndex;$i++)
+    {
+      print OUT $libResA[$i];
+    }
+    print OUT " ";
+    for(my $i=0;$i<$resBIndex;$i++)
                 {
-                        print OUT @libResB[$i];
+                        print OUT $libResB[$i];
                 }
-		print OUT "\n";
-		next;
-	}
-	if($resAlignIndex > 0)
-	{
-		my $quResID=substr($line,29,5);
-		my $libResType=changeAA(substr($line,18,3));
-		if( $chainA eq substr($line,27,1))
-		{
-			my $idx=searchIndex($quResID, $resAIndex, @resAArray);
-			$libChA=substr($line,9,1);
-			@libResA[$idx]=$libResType;
-		}
-		if ( $chainB eq substr($line,27,1))
-		{
-			my $idx=searchIndex($quResID, $resBIndex, @resBArray);
-			$libChB=substr($line,9,1);
-			@libResB[$idx]=$libResType;
-		}
-	}	
+    print OUT "\n";
+    next;
+  }
+  if($resAlignIndex > 0)
+  {
+    my $quResID=substr($line,29,5);
+    my $libResType=changeAA(substr($line,18,3));
+    if( $chainA eq substr($line,27,1))
+    {
+      my $idx=searchIndex($quResID, $resAIndex, @resAArray);
+      $libChA=substr($line,9,1);
+      $libResA[$idx]=$libResType;
+    }
+    if ( $chainB eq substr($line,27,1))
+    {
+      my $idx=searchIndex($quResID, $resBIndex, @resBArray);
+      $libChB=substr($line,9,1);
+      $libResB[$idx]=$libResType;
+    }
+  }  
 
 }
 
 sub changeAA {
-        my $triName=@_[0];
-        switch ($triName){
-                case "ALA"  {return 'A'}
-                case "ARG"  {return 'R'}
-                case "ASP"  {return 'D'}
-                case "ASN"  {return 'N'}
-                case "CYS"  {return 'C'}
-                case "GLN"  {return 'Q'}
-                case "GLY"  {return 'G'}
-                case "GLU"  {return 'E'}
-                case "HIS"  {return 'H'}
-                case "ILE"  {return 'I'}
-                case "LEU"  {return 'L'}
-                case "LYS"  {return 'K'}
-                case "MET"  {return 'M'}
-                case "PHE"  {return 'F'}
-                case "PRO"  {return 'P'}
-                case "SER"  {return 'S'}
-                case "THR"  {return 'T'}
-                case "TRP"  {return 'W'}
-                case "TYR"  {return 'Y'}
-                case "VAL"  {return 'V'}
-        }
+  my $triName=$_[0];
+  if($triName eq "ALA"){return 'A';}
+  elsif($triName eq "CYS"){return 'C';}
+  elsif($triName eq "ASP"){return 'D';}
+  elsif($triName eq "GLU"){return 'E';}
+  elsif($triName eq "PHE"){return 'F';}
+  elsif($triName eq "GLY"){return 'G';}
+  elsif($triName eq "HIS"){return 'H';}
+  elsif($triName eq "ILE"){return 'I';}
+  elsif($triName eq "LYS"){return 'K';}
+  elsif($triName eq "LEU"){return 'L';}
+  elsif($triName eq "MET"){return 'M';}
+  elsif($triName eq "ASN"){return 'N';}
+  elsif($triName eq "PRO"){return 'P';}
+  elsif($triName eq "GLN"){return 'Q';}
+  elsif($triName eq "ARG"){return 'R';}
+  elsif($triName eq "SER"){return 'S';}
+  elsif($triName eq "THR"){return 'T';}
+  elsif($triName eq "VAL"){return 'V';}
+  elsif($triName eq "TRP"){return 'W';}
+  elsif($triName eq "TYR"){return 'Y';}
 }
 
 sub searchIndex {
-	my $query=@_[0];
-	my $num=@_[1];
-	for(my $i=0;$i<$num;$i++)
-	{
-		if($query eq @_[$i+2])
-		{	
-			return $i;
-		}
-	}
-	die "can't find residue ".$query;
+  my $query=$_[0];
+  my $num=$_[1];
+  for(my $i=0;$i<$num;$i++)
+  {
+    if($query eq $_[$i+2])
+    {  
+      return $i;
+    }
+  }
+  die "can't find residue ".$query;
 }
 
 
